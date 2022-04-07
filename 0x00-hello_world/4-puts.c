8e0dd2d8116828208dd5379267ac4f92f52ec599
Emmanuel Ojo <ojoemmanuel935@gmail.com>
2022-04-07 15:46:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle"); return(0);
+#include <stdio.h>
+
+/**
+ * main -Entry point, print Hello, World
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
